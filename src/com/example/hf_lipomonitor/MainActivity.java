2c5ab335de1c4dd380578ac1214a8d2acf2cd006
Maciek <brayant@op.pl>
2015-02-11 21:57:14 +0100

    Added neccessary bluetooth permissions to AndroidManifesst.xml, added turning on bluetooth when app stars

--- src/com/example/hf_lipomonitor/MainActivity.java
+++ src/com/example/hf_lipomonitor/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hf_lipomonitor;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,6 +11,8 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
 
 
 
@@ -16,7 +20,7 @@ public class MainActivity extends Activity {
 	private EditText et;
 	private TextView tv;
 	private Context context;
-
+	private BluetoothSPP bt;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,9 +30,26 @@ public class MainActivity extends Activity {
 		
 		//temp code fragment using for tests
 		context = getApplicationContext();
+		bt = new BluetoothSPP(context);		
+		initBT();
 		
 		
 	}
+
+	private void initBT() {
+		if(!bt.isBluetoothEnabled()){
+			setBluetooth(true);
+			makeToast("Turning on Bluetooth...");
+		}
+	}
+	
+	@Override	
+	protected void onDestroy(){
+		super.onDestroy();
+		setBluetooth(false);
+		makeToast("TurnedOFF Bluetooth");
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,14 +67,39 @@ public class MainActivity extends Activity {
 		
 		switch (item.getItemId()){
 		case R.id.btSearch:
-
-
+			
 			Toast toast = Toast.makeText(context, "DUAP", 2);
-			toast.show();
+			toast.show();			
 			return true;
 			default:
 				return super.onOptionsItemSelected(item);
 		}
 		
+	}
+	
+	private static boolean setBluetooth(boolean enable) {
+	    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	    boolean isEnabled = bluetoothAdapter.isEnabled();
+	    if (enable && !isEnabled) {
+	        return bluetoothAdapter.enable(); 
+	    }
+	    else if(!enable && isEnabled) {
+	        return bluetoothAdapter.disable();
+	    }
+	    // No need to change bluetooth state
+	    return true;
+	}
+	
+	private void findAndSelectBTDevice(){
+		bt.startService(BluetoothState.DEVICE_OTHER);
+		
+	}
+	
+	//function mostly for debugging
+	public void makeToast(String text){
+		Context context = getApplicationContext();		
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
 	}
 }
