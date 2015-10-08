56c9072d6d1e70555cd985fbd0fc70d23913beb9
mwitas <bolek117@interia.pl>
2015-10-08 22:57:32 +0200

    Mobile v2 deleted from repository

--- mobile_v2/app/src/main/java/com/simonguest/btxfr/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.simonguest.btxfr;
-
-class Constants {
-    protected static final int CHUNK_SIZE = 4192;
-    protected static final int HEADER_MSB = 0x10;
-    protected static final int HEADER_LSB = 0x55;
-    protected static final String NAME = "ANDROID-BTXFR";
-    protected static final String UUID_STRING = "b7eba3af-ecb7-4195-8302-851ca790a00a";
-}
